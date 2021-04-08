45a9251243859d6c341fb9c276a842fcbb6d2798
erkitammeaid <erkipoiss01@gmail.com>
2021-04-08 14:56:08 +0300

    ssssssssssss

---
+++ controllers/TasksController.php
@@ -0,0 +1,54 @@
+<?php
+
+class TasksController {
+
+    public function index () {
+
+        global $app;
+
+        $tasks = $app['database']->selectAll('todos');
+
+        return view('tasks', ['tasks' => $tasks]);
+
+    }
+
+    public function store () {
+
+        global $app;
+
+        $app['database']->insert('todos', [
+            'description' => $_POST['description'],
+        ]);
+        
+        return redirect('tasks');
+    }
+
+    public function edit () {
+
+        global $app;
+
+        $task = $app['database']->selectById('todos', $_GET['id']);
+
+        return view('edit-task', ['task' => $task]);
+    }
+
+    public function update () {
+
+        global $app;
+
+        $app['database']->update('todos', $_POST['id'], [
+            'description' => $_POST['description'],
+        ]);
+
+        return redirect('tasks');
+    }
+
+    public function delete () {
+
+        global $app;
+
+        $app['database']->delete('todos', $_GET['id']);
+        
+        return redirect('tasks');
+    }    
+}
---
+++ views/tasks.view.php
@@ -0,0 +1,19 @@
+<?php require 'partials/head.php'; ?>
+
+    <ul>
+        <?php foreach ( $tasks as $task ) : ?>
+            <li>
+                <input type="checkbox" name="completed" <?= $task->completed == "1"  ? "checked" : ""; ?>>
+                <?= $task->description; ?>
+                <a href="/tasks/delete?id=<?= $task->id; ?>">delete</a>
+                <a href="/tasks/edit?id=<?= $task->id; ?>">edit</a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+
+    <form action="/tasks" method="POST">
+        <input type="text" name="description">
+        <button type="submit">Submit</button>
+    </form>
+
+<?php require 'partials/footer.php'; ?>
